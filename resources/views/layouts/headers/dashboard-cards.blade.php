45ae1005637a92fcb3e57ce866aa8787408f8f45
jeremykenedy <jeremykenedy@gmail.com>
2019-08-18 23:09:31 -0700

    start readme and update styleci config

--- resources/views/layouts/headers/dashboard-cards.blade.php
+++ resources/views/layouts/headers/dashboard-cards.blade.php
@@ -1,4 +1,4 @@
-<div class="header bg-gradient-primary pb-8 pt-5 pt-md-8 mt--7">
+<div class="header bg-gradient-primary pb-8 pt-5 pt-md-8 mt--5 mt-md--7">
     <div class="container-fluid">
         <div class="header-body mt-md-1">
             <div class="row">
--- resources/views/layouts/headers/spaced.blade.php
+++ resources/views/layouts/headers/spaced.blade.php
@@ -1,4 +1,4 @@
-<div class="header bg-gradient-gray pb-8 pt-5 pt-md-8 mt--7">
+<div class="header bg-gradient-gray pb-8 pt-5 pt-md-8 mt--5 mt-md--7">
     <div class="container-fluid">
         <div class="header-body mt-md-4 mt-lg-5">
         </div>
